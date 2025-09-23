83b1b60197c5a99ece269da485f81722d2779043
AllanDC1 <adonetticalen@gmail.com>
2025-09-23 10:08:44 -0300

    Initial commit

---
+++ src/test/java/com/allandc/ecommerce/SpringbootEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allandc.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
